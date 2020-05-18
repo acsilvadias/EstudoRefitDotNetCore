43dc98c2364555fd6911684a3c13f6b21954c6a0
Antonio Carlos <avanade.antonio@voeazul.com.br>
2020-05-18 11:01:10 -0300

    [EstudoRefitDotNet] Exemplo em Console Dot.Net Core

---
+++ EstudoRefitDotNetCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using EstudoRefitDotNetCore.Service.Interface;
+using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace EstudoRefitDotNetCore
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cepClient = RestService.For<ICepApiExemplo>("http://viacep.com.br");
+            string meuCep = "52130220";
+            Console.WriteLine("Consultando  Infos do CEP: {0}", meuCep);
+
+            var addess = await cepClient.GetAddressAsynnc(meuCep);
+
+            Console.Write($"\nLogradouro:{addess.Logradouro}\nBrairro:{addess.Bairro}\nEstado:{addess.Uf}\nCódigo Ibge:{addess.Ibge}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ EstudoRefitDotNetCore/Service/Interface/ICepApiExemplo.cs
@@ -0,0 +1,13 @@
+﻿using EstudoRefitDotNetCore.RefitExemplo;
+using Refit;
+using System.Threading.Tasks;
+
+
+namespace EstudoRefitDotNetCore.Service.Interface
+{
+    public interface ICepApiExemplo
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsynnc(string cep);
+    }
+}
